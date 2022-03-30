00d50fb758044ba40b4b77cec9c18bc08aa09f0c
nemanjapavloviic <nemanjapavloviic@gmail.com>
2022-03-30 12:48:03 +0200

    header for mobile and tablet added

--- header.php
+++ header.php
@@ -13,7 +13,9 @@
     <div id="page" class="site">
 
         <header>
-            <img src="<?= get_template_directory_uri() ?>/images/header.png" alt="">
+            <img class="mobile" src="<?= get_template_directory_uri() ?>/images/mobile-header.png" alt="">
+            <img class="tablet" src="<?=get_template_directory_uri() ?>/images/tablet-header.png" alt="">
+            <img class="desktop" src="<?= get_template_directory_uri() ?>/images/header.png" alt="">
             <a href="<?= site_url() ?>" class="logo"></a>
             <div class="popup-button"></div>
         </header>
